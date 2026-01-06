ff1ec8a3da63e14aa70fb54acf4c102c917baba1
fzk <fzk@fzk.fzk>
2026-01-06 20:33:53 +0100

    fix 404 error

--- telemetry/collector/src/main/java/ru/yandex/practicum/controller/EventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/controller/EventController.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.model.hub.HubEvent;
 import ru.yandex.practicum.model.sensor.SensorEvent;
 import ru.yandex.practicum.service.EventService;
 
+@RestController
 @RequestMapping("/events")
 public class EventController {
 
